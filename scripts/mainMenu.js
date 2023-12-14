3712cd23abe6e8c8e4d6245b8df723c637ef95a1
Naofumu <125041021+Naofumu@users.noreply.github.com>
2023-12-14 20:12:26 +0300

    SettingsWindow on Main Menu

--- scripts/mainMenu.js
+++ scripts/mainMenu.js
@@ -96,3 +96,22 @@ window.addEventListener('scroll', () => {
 })
 
 }
+
+let settingsButton = document.querySelector('.settingsButton')
+let settings = document.querySelector('.settingsContainer')
+let closeSettings = document.querySelector('.home')
+let levels = document.querySelector('.levels')
+
+settingsButton.onclick = function() {
+  settings.style.display = (settings.style.display != 'block') ? 'block' : 'none'
+}
+
+closeSettings.onclick = function() {
+  settings.style.display = 'none'
+}
+
+document.addEventListener('keydown', function(e){
+  if (e.key === 'Escape') {
+    settings.style.display = 'none'
+  }
+})
